bf72d5158168ce204a836597f0efae0de09a1f1a
Joris Lammers <joris.lammers.be@gmail.com>
2015-11-24 13:13:42 +0100

    Added basic command line argument parsing

--- ppsnr.go
+++ ppsnr.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
+var flagWidth int
+var flagHeight int
+var flagYuvRef string
+var flagYuvCompr string
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s -w WIDTH -h HEIGHT -r REF_YUV -c COMPR_YUV\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.IntVar(&flagWidth, "w", 1280, "Width of video")
+	flag.IntVar(&flagHeight, "h", 720, "Height of video")
+	flag.StringVar(&flagYuvRef, "r", "input.yuv", "Reference YUV")
+	flag.StringVar(&flagYuvCompr, "c", "output.yuv", "Compressed/Output YUV")
+}
+
 func main() {
-	fmt.Printf("Test\n")
+	flag.Parse()
 }
