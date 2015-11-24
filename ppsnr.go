5de22f821cd1e197b00367b91d57b1cba2783d41
Joris Lammers <joris.lammers.be@gmail.com>
2015-11-24 18:41:08 +0100

    Non-parallel PSNR calculation implemented

--- ppsnr.go
+++ ppsnr.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
+	"runtime"
 )
 
 var flagWidth int
@@ -22,6 +24,54 @@ func init() {
 	flag.StringVar(&flagYuvCompr, "c", "output.yuv", "Compressed/Output YUV")
 }
 
+func getInAndOutFrames(inFile, outFile *os.File, frameSize int) (inFrames, outFrames int) {
+	inFileStat, _ := inFile.Stat()
+	inFrames = int(inFileStat.Size() / int64(frameSize))
+	outFileStat, _ := outFile.Stat()
+	outFrames = int(outFileStat.Size() / int64(frameSize))
+	return
+}
+
+func psnr(frameNr int, YR, YC []byte) (psnrValue float64) {
+	var noise float64
+	for n := 0; n < len(YR); n++ {
+		yr := float64(YR[n])
+		yc := float64(YC[n])
+		noise += (yr - yc) * (yr - yc)
+	}
+	if noise == 0 {
+		psnrValue = 100
+	} else {
+		psnrValue = 10 * math.Log10(255.0*255.0*float64(len(YR))/noise)
+	}
+	return
+}
+
 func main() {
 	flag.Parse()
+	frameSize := (flagWidth * flagHeight * 3) / 2
+	fmt.Printf("Number of CPU cores %d\n", runtime.NumCPU())
+	fmt.Printf("YUV frame size is %d bytes\n", frameSize)
+	inFile, _ := os.Open(flagYuvRef)
+	outFile, _ := os.Open(flagYuvCompr)
+	inFrames, outFrames := getInAndOutFrames(inFile, outFile, frameSize)
+	var framesToCompare int
+	if inFrames < outFrames {
+		framesToCompare = inFrames
+	} else {
+		framesToCompare = outFrames
+	}
+
+	fmt.Printf("In: %d frames, Out: %d frames => Compare %d frames\n", inFrames, outFrames, framesToCompare)
+
+	for n := 0; n < framesToCompare; n++ {
+		YR := make([]byte, (frameSize*2)/3)
+		YC := make([]byte, (frameSize*2)/3)
+		inFile.Read(YR)
+		outFile.Read(YC)
+		p := psnr(n, YR, YC)
+		inFile.Seek(int64(frameSize/3), 1)
+		outFile.Seek(int64(frameSize/3), 1)
+		fmt.Printf("PSNR for frame %d is %3.2f\n", n, p)
+	}
 }
